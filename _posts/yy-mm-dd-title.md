03f92326573b3bd2b16797ced6b26a693fc30de3
dlehdrjsg <168471625+truebrid@users.noreply.github.com>
2025-07-06 04:21:43 +0900

    Initial commit: Jekyll Chirpy blog setup

---
+++ _posts/yy-mm-dd-title.md
@@ -0,0 +1,13 @@
+---
+title: # 포스트 제목
+description: # 포스트 설명 
+author: Beef # 작성자 이름
+date: # 작성 날짜 (YYYY-MM-DD HH:MM:SS +TIMEZONE 형식)
+categories: [] # 카테고리 목록
+tags: [] # 태그 목록 
+pin: true # 고정 여부 
+math: true # 수학 공식 렌더링 활성화
+mermaid: true # Mermaid 다이어그램 활성화
+image:
+  path: /assets/img/commons/ # 포스트 대표 이미지 경로
+---
---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,28 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1751743006',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
