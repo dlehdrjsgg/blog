31272d7b1aaf6633ef32d5e5b428d2a4a08e6211
dlehdrjsg <168471625+truebrid@users.noreply.github.com>
2025-07-06 04:27:44 +0900

    Remove GitHub Actions workflows

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751743006',resources: [
+    cacheName: 'chirpy-1751743618',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
